dfd6c7b97c3762e045855f81def1af6631834cf3
Alex Dewhirst <alexanderdewhirst@Alexs-MacBook-Pro.local>
2020-10-14 11:33:27 -0400

    feat: Add server concurrency in concurrent_listener.py

---
+++ concurrent_listener.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+
+import socket
+import selectors
+import types
+sel = selectors.DefaultSelector()
+
+HOST = '127.0.0.1'
+PORT = 4000
+
+lsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+lsock.bind((HOST, PORT))
+lsock.listen()
+lsock.setblocking(False)
+sel.register(lsock, selectors.EVENT_READ, data = None)
+
+def accept_wrapper(sock):
+    conn, addr = sock.accept()
+    print('Connected by', addr)
+    conn.setblocking(False)
+    data = types.SimpleNamespace(addr = addr, inb = b'', outb = b'')
+    events = selectors.EVENT_READ | selectors.EVENT_WRITE
+    sel.register(conn, events, data = data)
+
+def service_connection(key, mask):
+    sock = key.fileobj
+    data = key.data
+    if mask & selectors.EVENT_READ:
+        recv_data = sock.recv(1024)
+        if recv_data:
+            data.outb += recv_data
+        else:
+            print('closing connection to', data.addr)
+            sel.unregister(sock)
+            sock.close()
+    if mask & selectors.EVENT_WRITE:
+        if data.outb:
+            print('echoing', repr(data.outb), 'to', data.addr)
+            sent = sock.send(data.outb)
+            data.outb = data.outb[sent:]
+
+while True:
+    events = sel.select(timeout = None)
+    for key, mask in events:
+        if key.data is None:
+            accept_wrapper(key.fileobj)
+        else:
+            service_connection(key, mask)
