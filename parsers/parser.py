4cdbe8a57df7416811492802b7e6c8b11e79b611
Alex Dewhirst <alexanderdewhirst@Alexs-MacBook-Pro.local>
2020-10-14 12:25:35 -0400

    feat: Add parser errors for invalid arguments. doc: Add CLI help docs

--- parsers/parser.py
+++ parsers/parser.py
@@ -11,6 +11,7 @@ class Parser(object):
 
     def __call__(self):
         self.parsed_args = self.parser.parse_args()
+        self.validate_args()
         return self
 
     def _init_parser(self):
@@ -20,21 +21,42 @@ class Parser(object):
         Output:
             - Namespace (argparse object)
         """
-        parser = argparse.ArgumentParser()
+        parser = argparse.ArgumentParser(description = "Process file create/send/replace/delete and network connect/send/close actions.")
         subparsers = parser.add_subparsers(dest = 'command')
-        file_parser = subparsers.add_parser('file_manager')
+        file_parser = subparsers.add_parser('file_manager', help = "Process file actions.")
         file_parser.add_argument('-a', '--action', dest = 'action', type = str, action = 'store', help = 'Action.')
-        file_parser.add_argument('-f', '--filename', dest = 'file', type = str, action = 'store', help = 'Output file.')
+        file_parser.add_argument('-f', '--filename', dest = 'file', type = str, default = 'output.txt', action = 'store', help = 'Output file.')
         file_parser.add_argument('-d', '--data', dest = 'data', type = str, action = 'store', help = 'Data to store.')
         file_parser.add_argument('--replace-cell', dest = 'replace_cell', action = 'store_true', help = 'Replaces cell in csv.')
         file_parser.add_argument('-r', '--row', dest = 'row', type = int, action = 'store', help = 'Select row.')
         file_parser.add_argument('-c', '--column', dest = 'column', type = int, action = 'store', help = 'Select column.')
-        file_parser.add_argument('-l', '--log_filename', dest = 'log_file', type = str, action = 'store', help = 'Output log file.')
+        file_parser.add_argument('-l', '--log_filename', dest = 'log_file', type = str, default = 'logfile.txt', action = 'store', help = 'Output log file.')
 
-        network_parser = subparsers.add_parser('network_manager')
+        network_parser = subparsers.add_parser('network_manager', help = "Process network actions.")
         network_parser.add_argument('-a', '--action', dest = 'action', type = str, action = 'store', help = 'Action.')
         network_parser.add_argument('--host', dest = 'host', type = str, action = 'store', help = 'Remote host.')
         network_parser.add_argument('--port', dest = 'port', type = int, action = 'store', help = 'Server port.')
         network_parser.add_argument('-d', '--data', dest = 'data', type = str, action = 'store', help = 'Data to send.')
-        network_parser.add_argument('-l', '--log_filename', dest = 'log_file', type = str, action = 'store', help = 'Output log file.')
+        network_parser.add_argument('-l', '--log_filename', dest = 'log_file', type = str, default = 'logfile.txt', action = 'store', help = 'Output log file.')
         return parser
+
+    def validate_args(self):
+        if self.parsed_args.command not in ['file_manager', 'network_manager']:
+
+            self.parser.error("Command invalid. Must use file manager or network manager")
+        elif self.parsed_args.command == 'file_manager':
+            
+            if self.parsed_args.action == 'send' and not self.parsed_args.data:
+                self.parser.error("Must specify data to send.")
+
+            if self.parsed_args.action == "replace" and (not self.parsed_args.replace_data or not self.parsed_args.data):
+                self.parser.error("Must specify data being replaced and data to replace with.")
+
+        elif self.parsed_args.command == 'network_manager':
+
+            if not self.parsed_args.host or not self.parsed_args.port:
+                self.parser.error("Must specify host and port.")
+
+            if self.parsed_args.action == 'send' and not self.parsed_args.data:
+                self.parser.error("Must specify data to send.")
+            
